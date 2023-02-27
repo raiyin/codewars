1d56524f85aaec727b85f477f12e4b7518ffc366
Maxim P <raiyin@ya.ru>
2023-02-27 23:26:53 +0300

    Init js project.

--- js/8ku.js
+++ js/8ku.js
@@ -1,3 +1,10 @@
 function numberToString(num) {
     return num.toString();
 }
+
+const assert = require('chai').assert;
+describe("Tests", () => {
+    it("test", () => {
+        assert.strictEqual(numberToString(67), '67');
+    });
+});
