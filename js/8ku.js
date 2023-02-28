0e15e4b35277bfc8429c19f7ff8a78b911b41420
Maxim P <raiyin@ya.ru>
2023-02-28 22:26:54 +0300

    230228

--- js/8ku.js
+++ js/8ku.js
@@ -4,4 +4,23 @@ function numberToString(num) {
     return num.toString();
 }
 
+//523b4ff7adca849afe000035
+const greet = () => "hello world!";
+
+//53369039d7ab3ac506000467
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+}
+
+//5715eaedb436cf5606000381
+function positiveSum(arr) {
+    return arr.filter(item => item > 0).reduce((acc, currentValue) => acc + currentValue, 0);
+}
+
+//59ca8246d751df55cc00014c
+function hero(bullets, dragons) {
+    return (bullets >= dragons * 2) ? true : false;
+}
+
+
 assert.strictEqual(numberToString(67), '67', 'Numbers are not equal!');
