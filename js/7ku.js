c92a450f1c0340ee583ddada54782cd29c8e8b15
Maxim P <raiyin@ya.ru>
2023-03-05 18:26:37 +0300

    codewars_230305

--- js/7ku.js
+++ js/7ku.js
@@ -7,4 +7,13 @@ function disemvowel(str) {
     return str.replaceAll(regex, '');
 }
 
-console.log(disemvowel('This website is for lOsers LOL!'));
+//559590633066759614000063
+const minMax = arr => [Math.min(...arr), Math.max(...arr)];
+
+//5656b6906de340bd1b0000ac
+function longest(s1, s2) {
+    // 1. (s1, s2) => [...new Set(s1 + s2)].sort().join('')
+    return (s1 + s2).split('').filter((v, i, arr) => arr.indexOf(v) === i).sort().join('');
+}
+
+console.log(longest('aretheyhere'));
