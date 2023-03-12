31e1f095943eb49ff6002ff1e983a99c353626c0
Maxim P <raiyin@ya.ru>
2023-03-12 07:13:05 +0300

    codewars_230312

--- ts/8ku.ts
+++ ts/8ku.ts
@@ -14,3 +14,30 @@ export function rentalCarCost(d: number): number {
         rentOff = 20;
     return d * 40 - rentOff;
 }
+
+//57eae65a4321032ce000002d
+export const fakeBin = (x: string): string => {
+    return x.split('').map(item => (+item < 5 ? 0 : 1)).join('');
+};
+
+//58649884a1659ed6cb000072
+export function updateLight(current: string): string {
+    switch (current) {
+        case 'green':
+            return 'yellow';
+        case 'yellow':
+            return 'red';
+        default:
+            return 'green';
+    }
+    // 1. 
+    // const L = ['green', 'yellow', 'red', 'green']
+    // return L[L.indexOf(current)+1]
+    // 2.
+    //const objTraffic: Record<string,string> = {
+    //    "green" : "yellow",
+    //    "red": "green",
+    //    "yellow": "red"
+    //  }
+    //return objTraffic[current]
+}
