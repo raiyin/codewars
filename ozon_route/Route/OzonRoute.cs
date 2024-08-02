87f3dbcddb7364576c6a52b6f7b33115c09176c4
Maxim P. <raiyin@ya.ru>
2024-08-02 22:41:41 +0300

    Add two task from ozon route

--- ozon_route/Route/OzonRoute.cs
+++ ozon_route/Route/OzonRoute.cs
@@ -8,7 +8,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
-namespace Route
+namespace leetcode
 {
     internal class OzonRoute
     {
@@ -158,5 +158,178 @@ namespace Route
                 output.WriteLine(result);
         }
 
+        public void jsonPrettify()
+        {
+            using var input = new StreamReader(Console.OpenStandardInput());
+            using var output = new StreamWriter(Console.OpenStandardOutput());
+
+            var setCount = int.Parse(input.ReadLine());
+
+            for (int setIndex = 0; setIndex < setCount; setIndex++)
+            {
+                int stringCount = int.Parse(input.ReadLine());
+                var sb = new StringBuilder();
+
+                //for (int strIndex = 0; strIndex < stringCount; strIndex++)
+                //{
+                //    sb.Append(input.ReadLine());
+                //}
+                sb.Append("{\"a\": \"f\",\"b\": { \"c\": { \"d\": [], \"e\": [\"ababa\"]} },\"c\": { \"k\": { } },\"d\": { \"d\": { \"e\": { } } }}");
+
+                var options = new JsonSerializerOptions();
+                options.MaxDepth = 100;
+                var node = JsonNode.Parse(sb.ToString());
+                var items = (node as JsonObject).AsEnumerable().ToList();
+                foreach (var item in items)
+                {
+                    processNode(item.Key);
+                    items.Remove(item);
+                }
+
+            }
+
+            void processNode(JsonNode node)
+            {
+                if (node == null)
+                { }
+                else
+                {
+                }
+            }
+        }
+
+        public void PackingBoxes()
+        {
+            using var input = new StreamReader(Console.OpenStandardInput());
+            using var output = new StreamWriter(Console.OpenStandardOutput());
+
+            var setCount = int.Parse(input.ReadLine());
+            List<int> results = new List<int>();
+
+            for (int setIndex = 0; setIndex < setCount; setIndex++)
+            {
+                int transportation = 0;
+                var s = input.ReadLine().Split();
+                int carCount = int.Parse(s[0]);
+                int[] cars = new int[carCount];
+
+                int capacity = int.Parse(s[1]);
+                int[] degreesCounts = new int[(int)Math.Log2(capacity) + 1];
+
+                int boxCount = int.Parse(input.ReadLine());
+                List<int> degrees = input.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+                for (int i = 0; i < degrees.Count; i++)
+                {
+                    degreesCounts[degrees[i]]++;
+                }
+
+                while (degreesCounts.Any(item => item > 0))
+                {
+                    Array.Fill(cars, capacity);
+                    for (int carIndex = 0; carIndex < carCount; carIndex++)
+                    {
+                        for (int boxIndex = degreesCounts.Length - 1; boxIndex >= 0; boxIndex--)
+                        {
+                            if (degreesCounts[boxIndex] > 0 && cars[carIndex] - (int)Math.Pow(2, boxIndex) >= 0)
+                            {
+                                cars[carIndex] -= (int)Math.Pow(2, boxIndex);
+                                degreesCounts[boxIndex]--;
+                                boxIndex++;
+                            }
+                        }
+                    }
+                    transportation++;
+                }
+                results.Add(transportation);
+            }
+
+            foreach (var result in results)
+                output.WriteLine(result);
+        }
+
+        public void SimilarLogins()
+        {
+            using var input = new StreamReader(Console.OpenStandardInput());
+            using var output = new StreamWriter(Console.OpenStandardOutput());
+
+            var empLogins = new List<string>();
+            var canLogins = new List<string>();
+
+            var employeesCount = int.Parse(input.ReadLine());
+            for (int i = 0; i < employeesCount; i++)
+            {
+                empLogins.Add(input.ReadLine());
+            }
+
+            var candidatesCount = int.Parse(input.ReadLine());
+            int[] results = new int[candidatesCount];
+
+            for (int i = 0; i < candidatesCount; i++)
+            {
+                canLogins.Add(input.ReadLine());
+            }
+
+            for (int i = 0; i < candidatesCount; i++)
+            {
+                for (int j = 0; j < employeesCount; j++)
+                {
+                    if (canLogins[i] == empLogins[j])
+                    {
+                        results[i] = 1;
+                        break;
+                    }
+                    else if (canLogins[i].Length == empLogins[j].Length)
+                    {
+                        int loginLen = canLogins[i].Length;
+                        int diffCount = 0;
+                        bool isDiff = false;
+
+                        for (int cIndex = 0; cIndex < loginLen; cIndex++)
+                        {
+                            // Главная проверка на транспозицию.
+                            if (canLogins[i][cIndex] != empLogins[j][cIndex])
+                            {
+                                if ((cIndex != loginLen - 1))
+                                {
+                                    if (canLogins[i][cIndex] == empLogins[j][cIndex + 1] &&
+                                        canLogins[i][cIndex + 1] == empLogins[j][cIndex])
+                                    {
+                                        diffCount++;
+                                        cIndex++;
+                                    }
+                                    else
+                                    {
+                                        // Не разрешается транспозицией. Не похож.
+                                        isDiff = true;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    // Разница в последнем символе, не разрешается транспозицией. Не похож.
+                                    isDiff = true;
+                                    break;
+                                }
+                            }
+
+                            if (diffCount > 1)
+                            {
+                                isDiff = true;
+                                break;
+                            }
+                        }
+
+                        if (!isDiff)
+                        {
+                            results[i] = 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            foreach (var result in results)
+                output.WriteLine(result);
+        }
     }
 }
